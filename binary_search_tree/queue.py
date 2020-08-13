1b704b306d89373915f108a9bf366f61b80128f9
Riley <rrtoinfinity@gmail.com>
2020-08-13 13:50:26 -0400

    MVP I think

--- binary_search_tree/queue.py
+++ binary_search_tree/queue.py
@@ -1,7 +1,6 @@
 """
 A queue is a data structure whose primary purpose is to store and
 return elements in First In First Out order. 
-
 1. Implement the Queue class using an array as the underlying storage structure.
    Make sure the Queue tests pass.
 2. Re-implement the Queue class, this time using the linked list implementation
@@ -13,7 +12,6 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
-
 
 from singly_linked_list import LinkedList
 class Queue:
